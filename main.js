6e91f16ff65de8b2cda019c1d11a7f39ea1f57ae
otariterterashvili <otar.terterashvili.1@btu.edu.ge>
2020-12-24 22:01:25 +0400

    feat: js fundamentals

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("hello world")
+const x = [1, 2, 3, 4, 5]
+
+// x.forEach(function (i) {
+//   console.log(i)
+// })
+//
+const test = {
+  price: "12",
+  test: 12,
+}
+
+console.log(x)
+
+x.forEach((i) => console.log(i))
+
+t = x.map((i) => (i += 2))
+
+console.log(t)
