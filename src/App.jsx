0122e8f06ccbe3aa4a8667975e98a1951542e62f
otariterterashvili <otar.terterashvili.1@btu.edu.ge>
2021-02-07 22:05:18 +0400

    feat: http request and dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,51 @@
 import { Component } from "react"
-import FormikForm from "./components/FormikForm"
-import HooksForm from "./components/HooksForm"
-import ReactSelect from "./components/ReactSelect"
+import { createGlobalStyle, ThemeProvider } from "styled-components"
+import Products from "./components/Products"
+
+const darkTheme = {
+  fc: "#f9f7f7",
+  bg: "#112d4e",
+  card: "#dbe2ef",
+}
+const lightTheme = {
+  fc: "#112d4e",
+  bg: "#f9f7f7",
+  card: "#ffffff",
+}
+
+const GlobalStyle = createGlobalStyle`
+  body{
+    background-color: ${(props) => props.theme.bg}
+  }
+`
 
 class App extends Component {
   constructor() {
     super()
+    this.state = {
+      darkMode: localStorage.getItem("smart-dark") || false,
+    }
+    this.changeTheme = this.changeTheme.bind(this)
   }
-
+  changeTheme() {
+    localStorage.setItem("smart-dark", !this.state.darkMode)
+    this.setState({
+      darkMode: !this.state.darkMode,
+    })
+  }
   render() {
+    const { darkMode } = this.state
+    const theme = darkMode ? darkTheme : lightTheme
     return (
-      <main>
-        <FormikForm />
-        <HooksForm />
-        <ReactSelect />
-      </main>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <button onClick={this.changeTheme}>
+          {!darkMode ? "Dark" : "Light"} mode
+        </button>
+        <main>
+          <Products />
+        </main>
+      </ThemeProvider>
     )
   }
 }
